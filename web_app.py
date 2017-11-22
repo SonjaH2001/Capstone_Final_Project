36d1334b397b4cc9e0ebf03fc89d58a8f43ff744
SH <email@email.com>
2017-11-22 16:28:19 -0600

    fun with html page, just for practice. added home page route to web_app.py created splash page

--- web_app.py
+++ web_app.py
@@ -1,2 +1,16 @@
 # this is the main file that runs da biz.
 # The entrypoint, the runner.  banana
+
+from flask import Flask,render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')#route to home page
+def index():
+    return render_template('fun_time.html')
+
+
+if __name__ == '__main__':
+    # DB_orm.DB_setup()
+    # DB_manager.create_db()
+    app.run(debug=True) #kind of like the web server
