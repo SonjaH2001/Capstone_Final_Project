6a8a590926025d8644d87da7e6750bfacdbcd71c
SH <email@email.com>
2017-12-01 13:35:32 -0600

    updated entire concept. updated Main Page added star Id submit info very skeletal

---
+++ db_manager.py
@@ -0,0 +1,17 @@
+def find_daily_tutors():
+    daily_tutors = [{
+        "name": "mason",
+        "courses": "ITEC 1050, 1100, 1234",
+        "availability": "2 - 6"
+    },
+        {
+            "name": "bhjghj",
+            "courses": "ITEC 1050, 1100, 1234",
+            "availability": "2 - 6"
+        },
+        {
+            "name": "werwer",
+            "courses": "ITEC 1050, 1100, 1234",
+            "availability": "2 - 6"
+        }, ]
+    return daily_tutors
--- web_app.py
+++ web_app.py
@@ -3,12 +3,19 @@
 
 from flask import Flask,render_template, request
 
+from db_manager import find_daily_tutors
+
 app = Flask(__name__)
 
 @app.route('/')#route to home page
 def index():
-    return render_template('fun_time.html')
+    tutors = find_daily_tutors()#gets the array of dictionaries that have the data
+    return render_template('Main Display.html', tutors=tutors)#displays the data on the html page
 
+@app.route('/student_id')
+def student_sign_in(): #route to this page
+
+    return render_template('student_id.html')
 
 if __name__ == '__main__':
     # DB_orm.DB_setup()
